5bb0eb8b12733db78fc06ca18e7ff6de3c57a0f6
dinesh4900 <dineshm4900@gmail.com>
2021-06-04 19:21:24 +0530

    connected to frontend added pusher

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const Pusher = require("pusher");
 
 // import express from "express";
 
@@ -15,6 +16,14 @@ const port = process.env.port || 4001
 app.use(express.json())
 app.use(cors())
 
+const pusher = new Pusher({
+    appId: "1214565",
+    key: "1a7176f1457866160bef",
+    secret: "6d4cd69e962ed723ea14",
+    cluster: "ap2",
+    useTLS: true
+  });
+
 //db config
 const mongoURI = 'mongodb+srv://admin:HelloWorld@discord-mern.j8h2s.mongodb.net/discordDB?retryWrites=true&w=majority'
 
@@ -24,7 +33,28 @@ mongoose.connect(mongoURI, {
     useUnifiedTopology:true
 })
 
+mongoose.connection.once('open',()=>{
+    console.log('DB connected');
 
+    const changeStream = mongoose.connection.collection('conversations').watch()
+
+    changeStream.on('change',(change) => {
+        console.log('change');
+        if(change.operationType === 'insert'){
+            console.log('new channel');
+            pusher.trigger('channels','newChannel',{
+                'change':change
+            });
+        } else if(change.operationType === 'update') {
+            console.log('new message');
+            pusher.trigger('conversation', 'newMessage',{
+                'change': change
+            })
+        } else{
+            console.log('error in pusher');
+        }
+    })
+})
 
 // api routes
 app.get('/',(req,res) => res.status(200).send('hello world'))
@@ -63,7 +93,7 @@ app.get('/get/channelList',(req, res)=>{
 app.post('/new/message', (req, res)=>{
     const newMessage= req.body
 
-    mongoData.update(
+    mongoData.updateMany(
         {_id: req.query.id},
         {$push: {conversation: req.body}},
         (err,data)=>{
